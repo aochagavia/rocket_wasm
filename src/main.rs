9542d204fa2a821fc3e141003f40cf77192d012b
Adolfo Ochagavía <aochagavia92@gmail.com>
2017-11-30 19:58:26 +0100

    MVP
    `cargo check` says everything is all right, but generating wasm
    doesn't work because `rand` uses `libc`, and `libc` is not
    supported by wasm

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ mod geometry;
 mod models;
 mod util;
 
+use std::ffi::CString;
+use std::mem;
+use std::os::raw::{c_double, c_char, c_void};
 use std::sync::Mutex;
 use pcg_rand::Pcg32Basic;
 use rand::SeedableRng;
@@ -33,9 +36,19 @@ struct GameData {
     time_controller: TimeController<Pcg32Basic>
 }
 
-pub fn loop_(time: f64) -> String {
-    let data = DATA.lock().unwrap();
-    format!("Hello world")
+#[no_mangle]
+pub extern "C" fn update(time: c_double) -> *mut c_char {
+    CString::new(_update(time)).unwrap().into_raw()
+}
+
+pub fn _update(time: f64) -> String {
+    let data: &mut GameData = &mut DATA.lock().unwrap();
+    data.time_controller.update_seconds(time, data.input_controller.actions(), &mut data.state);
+    CollisionsController::handle_collisions(&mut data.state);
+
+    // Stats
+    let player_pos = data.state.world.player.vector.position;
+    format!("Player: {}, {}\nEnemies: {}", player_pos.x, player_pos.y, data.state.world.enemies.len())
 }
 
 // Comment this out, as it is definitely Piston-dependent
@@ -44,11 +57,6 @@ pub fn loop_(time: f64) -> String {
 pub fn main() {
     // Things we need to know from the browser:
     // * Update frame
-
-    // Things to keep in static memory
-    // * Game state
-    // * InputController
-    // * TimeController
 }
 
 // MVP
@@ -56,3 +64,26 @@ pub fn main() {
 // * No input
 // * 5 updates per second
 // * For each update, log some stats to the console
+
+// The usual boilerplate
+#[no_mangle]
+pub extern "C" fn alloc(size: usize) -> *mut c_void {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    ptr as *mut c_void
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc(ptr: *mut c_void, cap: usize) {
+    unsafe  {
+        let _buf = Vec::from_raw_parts(ptr, 0, cap);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc_str(ptr: *mut c_char) {
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    }
+}
